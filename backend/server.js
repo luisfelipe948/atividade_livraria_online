9bd68cb8ce31274a77fe36d46e509ca49ae51ed6
luisfelipe948 <luisdm.senac@gmail.com>
2026-05-27 22:35:47 -0300

    Corrigindo pasta backend

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+// IMPORTANDO AS ROTAS
+import routeClientes from "./src/routes/clienteRoutes.js";
+import routeCategoria from "./src/routes/categoriaRoutes.js";
+import routeEditora from "./src/routes/editoraRoutes.js";
+import routeCompra from "./src/routes/compraRoutes.js";
+import routeLivro from "./src/routes/livroRoutes.js";
+
+
+// CARREGAR AS VARIAVEIS DE AMBIENTE
+dotenv.config()
+
+// GARANTINDO QUE O SERVIDOR TENHA PORTA MESMO QUE NÃO ESTEJA DEFINIDO NO .ENV
+const PORT = process.env.PORT || 3000; 
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+// REGISTRANDO AS ROTAS
+app.use("/cliente", routeClientes);
+app.use("/categoria", routeCategoria);
+app.use("/compra", routeCompra);
+app.use("/editora", routeEditora);
+app.use("/livro", routeLivro);
+
+
+app.listen(PORT, () => {
+    return console.log(`Servidor rodando http://localhost:${PORT}`);
+  });
+  
---
+++ backend/src/database/connection.js
@@ -0,0 +1,12 @@
+import mysql from "mysql2/promise";
+import dotenv from "dotenv/config";
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST, // 127.0.0.1
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+export default pool;
